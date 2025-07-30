2baf78acfb3dd080c28944b872d940d97ae04dff
jonurce <jonur@stud.ntnu.no>
2025-07-30 16:34:52 +0200

    Add .gitignore for ROS 2 build artifacts

--- build/composition/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/jonurce/Jon/ROS2Projects/idea_0_ws/install/event_camera_codecs/share/event_camera_codecs/package.sh"
--- build/event_camera_codecs/CMakeFiles/event_camera_codecs_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for event_camera_codecs_uninstall.
-# This may be replaced when dependencies are built.
